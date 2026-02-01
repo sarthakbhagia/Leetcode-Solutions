35c53a50f2bde1d16edd333c2786722c4e5d41d7
Sarthak Bhagia <sarthakbhagia1@gmail.com>
2026-02-01 17:54:15 +0530

    Create 9.Palindrome-Number.cpp

---
+++ 9.Palindrome-Number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+       if (x < 0) {
+            return false;
+        }
+
+        long reverse = 0;
+        int t = x;
+
+        while (x > 0) {
+            reverse = (reverse * 10) + (x % 10);
+            x /= 10;
+        }
+
+        return reverse == t;    
+    }
+};
